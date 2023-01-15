5c03856e084128fc0637b20307f0b2d5b05fc3aa
Cesar-Mejia <chmejia1@gmail.com>
2023-01-15 15:15:59 -0500

    code refactor and add comments

--- algorithms.mjs
+++ algorithms.mjs
@@ -1,30 +1,37 @@
 export function bubbleSort(array) {
   const moves = []
   let noSwaps
+
   for (let i = array.length; i > 0; i--) {
     noSwaps = true
+
     for (let j = 0; j < i - 1; j++) {
       moves.push({ indeces: [j, j + 1], type: 'comp' })
+
       if (array[j] > array[j + 1]) {
         moves.push({ indeces: [j, j + 1], type: 'swap' })
         ;[array[j], array[j + 1]] = [array[j + 1], array[j]]
         noSwaps = false
       }
     }
+
     if (noSwaps) break
   }
+
   return moves
 }
 
 export function selectionSort(array) {
   let moves = []
+
   for (let i = 0; i < array.length; i++) {
     let lowest = i
+
     for (let j = i + 1; j < array.length; j++) {
       moves.push({ indeces: [i, lowest], j, type: 'comp' })
+
       if (array[lowest] > array[j]) {
         lowest = j
-        // moves.push({ indeces: [i, lowest], j, type: 'comp' })
       }
     }
 
@@ -40,6 +47,7 @@ export function selectionSort(array) {
 export function insertionSort(array) {
   let moves = []
   let currentVal
+
   for (let i = 1; i < array.length; i++) {
     currentVal = array[i]
     let j = i - 1
@@ -49,17 +57,17 @@ export function insertionSort(array) {
       moves.push({ indeces: [j, j + 1], type: 'comp' })
       moves.push({ indeces: [j + 1, j], type: 'swap' })
       ;[array[j + 1], array[j]] = [array[j], array[j + 1]]
-
       j--
     }
   }
+
   return moves
 }
 
 export function mergeSort(array, moves_m = []) {
   if (array.length <= 0) return array
-
   let auxiliaryArray = [...array]
+
   mergeSortHelper(array, 0, array.length - 1, auxiliaryArray, moves_m)
   return moves_m
 }
@@ -67,11 +75,13 @@ export function mergeSort(array, moves_m = []) {
 function mergeSortHelper(mainArray, startIdx, endIdx, auxiliaryArray, moves_m) {
   if (startIdx === endIdx) return
   let middleIdx = startIdx + Math.floor((endIdx - startIdx) / 2)
+
   mergeSortHelper(auxiliaryArray, startIdx, middleIdx, mainArray, moves_m)
   mergeSortHelper(auxiliaryArray, middleIdx + 1, endIdx, mainArray, moves_m)
   merge(mainArray, startIdx, middleIdx, endIdx, auxiliaryArray, moves_m)
 }
 
+// merge-sort helper function - sorts array in place given start/end indeces using auxilliary array
 function merge(mainArray, startIdx, middleIdx, endIdx, auxiliaryArray, moves_m) {
   let replace = []
   let k = startIdx
@@ -80,15 +90,19 @@ function merge(mainArray, startIdx, middleIdx, endIdx, auxiliaryArray, moves_m) 
 
   while (i <= middleIdx && j <= endIdx) {
     moves_m.push({ indeces: [i, j], type: 'comp' })
+
     if (auxiliaryArray[i] <= auxiliaryArray[j]) {
       mainArray[k] = auxiliaryArray[i]
+
       replace.push({ indeces: [k], newVal: auxiliaryArray[i], type: 'replace' })
       i++
     } else {
       mainArray[k] = auxiliaryArray[j]
+
       replace.push({ indeces: [k], newVal: auxiliaryArray[j], type: 'replace' })
       j++
     }
+
     k++
   }
 
@@ -105,20 +119,25 @@ function merge(mainArray, startIdx, middleIdx, endIdx, auxiliaryArray, moves_m) 
     j++
     k++
   }
+
   moves_m.push(...replace)
 }
 
+// quick-sort helper function - places first array item in correct (sorted) place and returns the index
 function pivot(arr, start = 0, end = arr.length - 1, moves_q) {
   let swapIdx = start
 
   for (let i = start + 1; i <= end; i++) {
     moves_q.push({ indeces: [start, i, swapIdx], start, swapIdx, type: 'comp' })
+
     if (arr[i] < arr[start]) {
       swapIdx++
       ;[arr[swapIdx], arr[i]] = [arr[i], arr[swapIdx]]
+
       moves_q.push({ indeces: [swapIdx, i], start, swapIdx, type: 'swap', finalSwap: false })
     }
   }
+
   ;[arr[start], arr[swapIdx]] = [arr[swapIdx], arr[start]]
   moves_q.push({ indeces: [swapIdx, start], start, swapIdx, type: 'swap', finalSwap: true })
   return swapIdx
@@ -127,6 +146,7 @@ function pivot(arr, start = 0, end = arr.length - 1, moves_q) {
 export function quickSort(arr, left = 0, right = arr.length - 1, moves_q = []) {
   if (left >= right) return
   let pivotIdx = pivot(arr, left, right, moves_q)
+
   quickSort(arr, left, pivotIdx - 1, moves_q)
   quickSort(arr, pivotIdx + 1, right, moves_q)
   return moves_q
--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 import { bubbleSort, selectionSort, insertionSort, mergeSort, quickSort } from './algorithms.mjs'
 
+// initialization of html elements to variables
 const container = document.getElementById('bars-container')
 const generateArrayBtn = document.getElementById('generate-array-btn')
 const sortBtn = document.getElementById('sort-btn')
@@ -7,12 +8,14 @@ const algoBtns = document.getElementById('algorithm-btns').children
 const speedSlider = document.getElementById('speed')
 const sizeSlider = document.getElementById('size')
 
+// event listener initialization
 window.addEventListener('load', onLoad)
 generateArrayBtn.addEventListener('click', generateArray)
 sortBtn.addEventListener('click', sort)
 speedSlider.oninput = speedSlider_init
 sizeSlider.oninput = sizeSlider_init
 
+// global values
 let totalBars = sizeSlider.value
 let animationDelay = speedSlider.max - speedSlider.value
 let array = []
@@ -24,11 +27,35 @@ let resetClicked = false
 
 // *********************** init *************************************** //
 
+function generateArray() {
+  // displays bars given current array
+  function showBars() {
+    container.innerHTML = ''
+    for (let i = 0; i < array.length; i++) {
+      const bar = document.createElement('div')
+      bar.style.height = array[i] + '%'
+      bar.classList.add('bar')
+      container.appendChild(bar)
+    }
+  }
+
+  sortBtn.disabled = false
+  sizeSlider.disabled = false
+
+  // reset array values
+  for (let i = 0; i < totalBars; i++) {
+    array[i] = Math.floor(Math.random() * 100)
+  }
+  showBars()
+  if (isAnimating) resetClicked = true
+}
+
 function onLoad() {
   algoBtns_init()
   generateArray()
 }
 
+// algorithm button initialization
 function algoBtns_init() {
   for (let btn of algoBtns) {
     if (btn.id === currentSelection) {
@@ -38,6 +65,7 @@ function algoBtns_init() {
     }
   }
 
+  // handles selection of new algorithms
   function selectAlgo(e) {
     generateArrayBtn.click()
     currentSelection = e.target.id
@@ -51,15 +79,18 @@ function algoBtns_init() {
     }
   }
 
+  // initialize event listeners for algorithm buttons
   for (let btn of algoBtns) {
     btn.addEventListener('click', e => selectAlgo(e))
   }
 }
 
+// speed slider initialization
 function speedSlider_init() {
   animationDelay = speedSlider.max - this.value
 }
 
+// size slider initialization
 function sizeSlider_init() {
   totalBars = this.value
   array = []
@@ -68,60 +99,13 @@ function sizeSlider_init() {
 
 // ******************************************************************** //
 
-function showBars() {
-  container.innerHTML = ''
-  for (let i = 0; i < array.length; i++) {
-    const bar = document.createElement('div')
-    bar.style.height = array[i] + '%'
-    bar.classList.add('bar')
-    container.appendChild(bar)
-  }
-}
-
-function generateArray() {
-  sortBtn.disabled = false
-  sizeSlider.disabled = false
-  for (let i = 0; i < totalBars; i++) {
-    array[i] = Math.floor(Math.random() * 100)
-  }
-  showBars()
-  if (isAnimating) resetClicked = true
-}
-
-// function sort() {
-//   isAnimating = true
-//   sortBtn.disabled = true
-//   sizeSlider.disabled = true
-
-//   switch (currentSelection) {
-//     case 'bubble-sort':
-//       const moves_bubble = bubbleSort([...array])
-//       animate_bubbleSort(moves_bubble)
-//       break
-//     case 'selection-sort':
-//       const moves_selection = selectionSort([...array])
-//       animate_selectonSort(moves_selection)
-//       break
-//     case 'insertion-sort':
-//       const moves_insertion = insertionSort([...array])
-//       animate_insertionSort(moves_insertion)
-//       break
-//     case 'merge-sort':
-//       const moves_merge = mergeSort([...array])
-//       animate_mergeSort(moves_merge)
-//       break
-//     case 'quick-sort':
-//       const moves_quick = quickSort([...array])
-//       animate_quickSort(moves_quick)
-//       break
-//   }
-// }
-
+// resets booleans after stopping animations
 function reset() {
   isAnimating = false
   resetClicked = false
 }
 
+// activates algorithm given currently selected algorithm
 function sort() {
   isAnimating = true
   sortBtn.disabled = true
@@ -146,215 +130,9 @@ function sort() {
   }
 }
 
-// ************************** Animations ****************************** //
-
-// function animate_bubbleSort(moves) {
-//   function showBars_bubbleSort(move) {
-//     container.innerHTML = ''
-//     for (let i = 0; i < array.length; i++) {
-//       const bar = document.createElement('div')
-//       bar.style.height = array[i] + '%'
-//       bar.classList.add('bar')
-
-//       if (move && move.indeces.includes(i) && move.type === 'swap') {
-//         bar.style.backgroundColor = i === move.indeces[0] ? color2 : color1
-//       } else if (move && move.indeces.includes(i) && move.type === 'comp') {
-//         bar.style.backgroundColor = i === move.indeces[1] ? color2 : color1
-//       }
-
-//       container.appendChild(bar)
-//     }
-//   }
-
-//   if (resetClicked) {
-//     reset()
-//     return
-//   }
-//   if (!moves.length) {
-//     reset()
-//     return showBars_bubbleSort()
-//   }
-
-//   const move = moves.shift()
-//   const [i, j] = move.indeces
-
-//   if (move.type === 'swap') {
-//     ;[array[i], array[j]] = [array[j], array[i]]
-//   }
-
-//   showBars_bubbleSort(move)
-//   setTimeout(() => {
-//     animate_bubbleSort(moves)
-//   }, animationDelay)
-// }
-
-// function animate_selectonSort(moves) {
-//   function showBars_selectionSort(move) {
-//     container.innerHTML = ''
-//     for (let i = 0; i < array.length; i++) {
-//       const bar = document.createElement('div')
-//       bar.style.height = array[i] + '%'
-//       bar.classList.add('bar')
-
-//       if (move && move.indeces.includes(i)) {
-//         bar.style.backgroundColor = move.type = 'comp' ? color2 : color1
-//       }
-//       if (move && move.j === i) {
-//         bar.style.backgroundColor = color1
-//       }
-
-//       container.appendChild(bar)
-//     }
-//   }
-
-//   if (resetClicked) {
-//     reset()
-//     return
-//   }
-//   if (!moves.length) {
-//     reset()
-//     return showBars_selectionSort()
-//   }
-
-//   const move = moves.shift()
-//   const [i, j] = move.indeces
-
-//   if (move.type === 'swap') {
-//     ;[array[i], array[j]] = [array[j], array[i]]
-//   }
-
-//   showBars_selectionSort(move)
-//   setTimeout(() => {
-//     animate_selectonSort(moves)
-//   }, animationDelay)
-// }
-
-// function animate_insertionSort(moves) {
-//   function showBars_insertionSort(move) {
-//     container.innerHTML = ''
-//     for (let i = 0; i < array.length; i++) {
-//       const bar = document.createElement('div')
-//       bar.style.height = array[i] + '%'
-//       bar.classList.add('bar')
-
-//       if (move && move.indeces.includes(i) && move.type === 'comp') {
-//         bar.style.backgroundColor = move.indeces[0] === i ? color1 : color2
-//       }
-//       if (move && move.indeces.includes(i) && move.type === 'swap') {
-//         bar.style.backgroundColor = move.indeces[0] === i ? color1 : color2
-//       }
-
-//       container.appendChild(bar)
-//     }
-//   }
-
-//   if (resetClicked) {
-//     reset()
-//     return
-//   }
-//   if (!moves.length) {
-//     reset()
-//     return showBars_insertionSort()
-//   }
-
-//   const move = moves.shift()
-//   const [i, j] = move.indeces
-
-//   if (move.type === 'swap') {
-//     ;[array[i], array[j]] = [array[j], array[i]]
-//   }
-
-//   showBars_insertionSort(move)
-//   setTimeout(() => {
-//     animate_insertionSort(moves)
-//   }, animationDelay)
-// }
-
-// function animate_mergeSort(moves) {
-//   function showBars_mergeSort(move) {
-//     container.innerHTML = ''
-//     for (let i = 0; i < array.length; i++) {
-//       const bar = document.createElement('div')
-//       bar.style.height = array[i] + '%'
-//       bar.classList.add('bar')
-
-//       if (move && move.indeces.includes(i)) {
-//         bar.style.backgroundColor = move.type === 'comp' ? color1 : color2
-//       }
-
-//       container.appendChild(bar)
-//     }
-//   }
-
-//   if (resetClicked) {
-//     reset()
-//     return
-//   }
-//   if (!moves.length) {
-//     reset()
-//     return showBars_mergeSort()
-//   }
-
-//   let move = moves.shift()
-//   const [i] = move.indeces
-
-//   if (move.type === 'replace') {
-//     array[i] = move.newVal
-//   }
-
-//   showBars_mergeSort(move)
-//   setTimeout(() => {
-//     animate_mergeSort(moves)
-//   }, animationDelay)
-// }
-
-// function animate_quickSort(moves) {
-//   function showBars_quickSort(move) {
-//     container.innerHTML = ''
-//     for (let i = 0; i < array.length; i++) {
-//       const bar = document.createElement('div')
-//       bar.style.height = array[i] + '%'
-//       bar.classList.add('bar')
-
-//       if (move && move.type === 'swap' && (move.start === i || move.indeces.includes(i))) {
-//         if (!move.finalSwap) {
-//           bar.style.backgroundColor = move.start === i ? color1 : color2
-//         } else if (move.indeces.includes(i)) {
-//           bar.style.backgroundColor = color2
-//         }
-//       } else if (move && move.indeces.includes(i) && move.type === 'comp') {
-//         bar.style.backgroundColor = color1
-//       }
-//       if (move && i > move.start && i < move.swapIdx) {
-//         bar.style.backgroundColor = 'gray'
-//       }
-
-//       container.appendChild(bar)
-//     }
-//   }
-
-//   if (resetClicked) {
-//     reset()
-//     return
-//   }
-//   if (!moves.length) {
-//     reset()
-//     return showBars_quickSort()
-//   }
-
-//   const move = moves.shift()
-//   const [i, j] = move.indeces
-
-//   if (move.type === 'swap') {
-//     ;[array[i], array[j]] = [array[j], array[i]]
-//   }
-//   showBars_quickSort(move)
-//   setTimeout(() => {
-//     animate_quickSort(moves)
-//   }, animationDelay)
-// }
-
+// animates algorithm after moves are computed
 function animate(moves) {
+  // draws the bars depending on current algorithms selection
   function drawBars(move) {
     container.innerHTML = ''
     for (let i = 0; i < array.length; i++) {
